3db06892f62130335906fbece958b73cee3be918
Sebastian Saenz <ssaenz@multeo.net>
2019-07-01 20:36:23 -0300

    Botones para cambiar de idioma

--- app/components/Main.vue
+++ app/components/Main.vue
@@ -4,6 +4,11 @@
             <FlexboxLayout flexDirection="column" alignItems="center" justifyContent="center">
                 <Image class="image" src="res://icon" stretch="none" />
                 <Label class="h2 label" :text="$t('main:title')" />
+                <FlexboxLayout class="m-t-10" justifyContent="space-between">
+                    <Image class="image m-x-5" src="http://www.scopribrescia.com/wp-content/uploads/2016/02/Visitas-guiadas-en-espa%C3%B1ol-Christo-The-Floating-Piers.png" stretch="aspectFill" height="30" width="30" @tap="changeLocale('es')"/>
+                    <Image class="image m-x-5" src="https://cdn4.iconfinder.com/data/icons/flag_set/512/br.png" stretch="aspectFill" height="30" width="30" @tap="changeLocale('pt')"/>
+                    <Image class="image m-x-5" src="https://image.flaticon.com/icons/png/128/323/323329.png" stretch="aspectFill" height="30" width="30" @tap="changeLocale('en')"/>
+                </FlexboxLayout>
                 <Button class="button" :text="$t('main:button:start')" @tap="goToLessons" />
                 <ActivityIndicator class="m-t-15" :busy="loading" />
             </FlexboxLayout>
@@ -49,16 +54,15 @@ import { mapActions } from 'vuex'
                         }
                     })
                 }
+            },
+            changeLocale(locale) {
+                this.$i18n.locale = locale;
             }
         }
     };
 </script>
 
 <style scoped>
-    .image {
-
-    }
-
     .label {
         margin-top: 20;
     }
